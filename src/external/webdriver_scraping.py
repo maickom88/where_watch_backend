926b044428d82a7b3b9a2fcb2bc994b92132f84a
Michael Frank <maickomtutos@gmail.com>
2021-06-10 17:08:05 -0300

    change file

--- src/external/webdriver_scraping.py
+++ src/external/webdriver_scraping.py
@@ -184,7 +184,7 @@ def configure_set_up_driver() -> WebDriver:
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--window-size=1920x1080')
+    chrome_options.add_argument('window-size=1920x1080')
     chrome_options.add_argument('--no-sandbox')
     return webdriver.Chrome(
         ChromeDriverManager().install(), options=chrome_options)
