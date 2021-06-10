216ce6a83f459cf05758c169a76e0040529a662e
Michael Frank <maickomtutos@gmail.com>
2021-06-10 17:26:22 -0300

    add new file

--- src/external/webdriver_scraping.py
+++ src/external/webdriver_scraping.py
@@ -185,6 +185,7 @@ def configure_set_up_driver() -> WebDriver:
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('window-size=1920x1080')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--no-sandbox')
     return webdriver.Chrome(
         ChromeDriverManager().install(), options=chrome_options)
