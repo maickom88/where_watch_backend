19cb1acff0e78d0e6e7df57d7ebbf57d8bdb4361
Michael Frank <maickomtutos@gmail.com>
2021-06-10 17:08:40 -0300

    change windows size

--- src/external/webdriver_scraping.py
+++ src/external/webdriver_scraping.py
@@ -184,7 +184,7 @@ def configure_set_up_driver() -> WebDriver:
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--window-size=1920x1080')
+    chrome_options.add_argument('window-size=1920x1080')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     return webdriver.Chrome(
